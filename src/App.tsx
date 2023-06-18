1a4c84eccf38f643ece3569afb4fee464be7ba51
foobar <uagnl25x@anonaddy.me>
2023-06-18 21:52:16 +0530

    Changed function to arrow function on App()

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BookingsPagination } from './components/BookingsPagination'
+import { TitleBar } from './components/TitleBar'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-
-  )
+const App = () => {
+    return (
+        <>
+            <TitleBar />
+            <BookingsPagination />
+        </>
+    )
 }
 
 export default App
