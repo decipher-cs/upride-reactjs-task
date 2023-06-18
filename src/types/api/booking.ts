5a3dfcae635e2b9f46eb2d953b3016b2fa5e0002
foobar <uagnl25x@anonaddy.me>
2023-06-18 14:01:23 +0530

    Added types for api

---
+++ src/types/api/booking.ts
@@ -0,0 +1,39 @@
+export interface OnlineOfflineBookings {
+    offline_bookings: { [key: string]: Booking }
+    online_bookings: { [key: string]: Booking }
+}
+
+export interface Booking {
+    bookingAmount: number
+    bookingEpochTime: number
+    bookingID: string
+    bookingStatus: BookingStatus
+    bookingSuccessful: boolean
+    clientID: string
+    clientName: string
+    clientPhone: string
+    numSessions: number
+    offlineBooking: boolean
+    packageID: string
+    packageTitle: string
+    paymentID: string
+    paymentMode: string
+    paymentSuccessful: boolean
+    refundStatus: RefundStatus
+    userEmail: string
+    userGender: UserGender
+    userID: string
+    userName: string
+    userPhone: string
+    withDL?: boolean
+    workshopID: string
+    workshopImage: string
+    workshopTitle: string
+    staffID?: string
+}
+
+export type BookingStatus = 'SUCCESS' | 'CANCELLED' | 'COMPLETED'
+
+export type RefundStatus = 'REFUND_NOT_APPLICABLE'
+
+export type UserGender = 'Male' | 'Female' | 'M' | 'NA' | 'f'
