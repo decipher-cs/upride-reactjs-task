c6347301a336554f39f0144386e3f865b75b709c
foobar <uagnl25x@anonaddy.me>
2023-06-19 13:05:03 +0530

    Added filtering capabilites

--- src/components/BookingsPagination.tsx
+++ src/components/BookingsPagination.tsx
@@ -1,43 +1,23 @@
 import { Box, Container, List, ListItem, ListItemText, Pagination, Stack, Tab, Tabs, Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
-import { Booking, OnlineOfflineBookings } from '../types/api/booking'
+import { Booking, BookingStatus, OnlineOfflineBookings } from '../types/api/booking'
 
 interface BookingWithMedium extends Booking {
     bookingMedium: 'offline' | 'online'
 }
 
-interface TabPanelProps {
-    children?: React.ReactNode
-    index: number
-    value: number
-}
-
-function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props
-
-    return (
-        <div
-            role='tabpanel'
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{ p: 3 }}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    )
-}
-
 export const BookingsPagination = () => {
     const [totalPages, setTotalPages] = useState(10)
-    const [currPage, setCurrPage] = useState(2)
+
+    const [currPage, setCurrPage] = useState(1)
+
     const [bookings, setBookings] = useState<BookingWithMedium[]>([])
+
     const [itemsPerPage, setItemsPerPage] = useState(10)
+
     const [currTab, setCurrTab] = useState<0 | 1 | 2>(0)
+
+    const [currBookingFilter, setCurrBookingFilter] = useState<BookingStatus>('SUCCESS')
 
     useEffect(() => {
         const URL = import.meta.env.VITE_API_URL
@@ -49,6 +29,7 @@ export const BookingsPagination = () => {
         fetch(URL)
             .then(data => data.json())
             .then((data: OnlineOfflineBookings) => {
+                console.log(data)
                 const allBookings: BookingWithMedium[] = []
 
                 for (const objName in data.offline_bookings) {
@@ -58,47 +39,57 @@ export const BookingsPagination = () => {
                     allBookings.push({ bookingMedium: 'online', ...data.online_bookings[objName] })
                 }
                 allBookings.sort((a, b) => a.bookingEpochTime - b.bookingEpochTime)
-                // Add a functio to convert time to human readable form.
+
+                // TODO: Add a functio to convert time to human readable form.
+
                 setBookings(allBookings)
+                setTotalPages(Math.ceil(allBookings.length / 10))
             })
             .catch(err => {
                 console.log('Error while fetching data:', err)
             })
     }, [])
 
+    const handleBookingFilter = (selectedTab: 0 | 1 | 2) => {
+        if (selectedTab === 0) setCurrBookingFilter('SUCCESS')
+        else if (selectedTab === 1) setCurrBookingFilter('COMPLETED')
+        else if (selectedTab === 2) setCurrBookingFilter('CANCELLED')
+        setCurrTab(selectedTab)
+    }
+
     return (
         <Container>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Tabs value={currTab} onChange={(_, tab) => setCurrTab(tab)} aria-label='basic tabs example'>
+                <Tabs
+                    value={currTab}
+                    onChange={(_, tab) => {
+                        handleBookingFilter(tab)
+                    }}
+                    aria-label='Booking filter type'
+                >
                     <Tab label='Active' />
                     <Tab label='Completed' />
                     <Tab label='Cancelled' />
                 </Tabs>
             </Box>
-            <TabPanel value={currTab} index={0}>
-                Item One
-            </TabPanel>
-            <TabPanel value={currTab} index={1}>
-                Item Two
-            </TabPanel>
-            <TabPanel value={currTab} index={2}>
-                Item Three
-            </TabPanel>
-            <List>
-                {bookings
-                    .map((value, i) => {
-                        return (
-                            <ListItem key={i}>
-                                <ListItemText>{value.clientName}</ListItemText>
-                                <ListItemText>{value.bookingEpochTime}</ListItemText>
-                                <ListItemText>{value.packageID}</ListItemText>
-                                <ListItemText>{value.bookingMedium}</ListItemText>
-                            </ListItem>
-                        )
-                    })
-                    .slice((currPage - 1) * itemsPerPage, (currPage - 1) * itemsPerPage + itemsPerPage)}
-            </List>
-            <Pagination count={totalPages} page={currPage} onChange={(_, page) => setCurrPage(page)} />
+            <Box>
+                <List>
+                    {bookings
+                        .filter(booking => booking.bookingStatus === currBookingFilter)
+                        .slice((currPage - 1) * itemsPerPage, (currPage - 1) * itemsPerPage + itemsPerPage)
+                        .map((value, i) => {
+                            return (
+                                <ListItem key={i}>
+                                    <ListItemText>{value.clientName}</ListItemText>
+                                    <ListItemText>{value.bookingEpochTime}</ListItemText>
+                                    <ListItemText>{value.packageID}</ListItemText>
+                                    <ListItemText>{value.bookingMedium}</ListItemText>
+                                </ListItem>
+                            )
+                        })}
+                </List>
+                <Pagination count={totalPages} page={currPage} onChange={(_, page) => setCurrPage(page)} />
+            </Box>
         </Container>
     )
 }
