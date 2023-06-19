02ae7fb4f331ccf519cdb2bb5c02888bc2f01891
foobar <uagnl25x@anonaddy.me>
2023-06-19 16:41:55 +0530

    Added SidePanel component to App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BookingsPagination } from './components/BookingsPagination'
+import { SidePanel } from './components/SidePanel'
 import { TitleBar } from './components/TitleBar'
 
 const App = () => {
     return (
         <>
+            <SidePanel />
             <TitleBar />
             <BookingsPagination />
         </>
