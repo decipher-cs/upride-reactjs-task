57848e35982fc6b30ad47518a67cbcb73594eb4e
foobar <uagnl25x@anonaddy.me>
2023-06-20 14:17:25 +0530

    Upride logo added

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -13,7 +13,7 @@ import {
 } from '@mui/material'
 import React, { useState } from 'react'
 import AttractionsIcon from '@mui/icons-material/Attractions'
-import { Label } from '@mui/icons-material'
+import uprideLogo from '../assets/uprideLogo.svg'
 
 export const SidePanel = (props: { sx: SxProps }) => {
     const [selection, setSelection] = useState<string | null>('home')
@@ -23,11 +23,8 @@ export const SidePanel = (props: { sx: SxProps }) => {
     }
 
     return (
-        <Box sx={props.sx}>
-            <Typography variant='h5'>
-                <AttractionsIcon fontSize='medium' />
-                Upride
-            </Typography>
+        <Box sx={{ ...props.sx, gap: 5, display: 'grid', alignItems: 'center', justifyItems: 'center' }}>
+            <img src={uprideLogo} />
             <Card raised>
                 <CardContent>
                     <Typography>Rajarajeswari Nagar</Typography>
