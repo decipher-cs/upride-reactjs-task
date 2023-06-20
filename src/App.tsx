63a433b22435406c7b059ad81c3497b5d9cfac71
foobar <uagnl25x@anonaddy.me>
2023-06-20 14:18:35 +0530

    CSS to expand app to entire screen added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import { Box } from '@mui/material'
+import { Box, ThemeOptions, ThemeProvider, createTheme } from '@mui/material'
 import { BookingsPagination } from './components/BookingsPagination'
 import { SidePanel } from './components/SidePanel'
 import { TitleBar } from './components/TitleBar'
+import { red } from '@mui/material/colors'
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: red[500],
+        },
+    },
+})
 
 const App = () => {
     return (
-        <Box
-            display='flex'
-            sx={{
-                border: 'solid red 7px',
-                alignItems: 'stretch',
-            }}
-        >
-            <SidePanel sx={{ border: 'solid green 3px', flexBasis: '23%' }} />
-            <Box sx={{ flexBasis: '100%' }}>
-                <TitleBar sx={{}}/>
-                <BookingsPagination sx={{ height: 'auto' }} />
+        <ThemeProvider theme={theme}>
+            <Box display='flex' sx={{minHeight: '100vh'}}>
+                <SidePanel
+                    sx={{
+                        flexBasis: '23%',
+                        background: 'linear-gradient(180deg, #EB6B9D 0%, rgba(255, 87, 87, 0.7) 138.64%)',
+                    }}
+                />
+
+                <Box sx={{ flexBasis: '100%' }}>
+                    <TitleBar sx={{}} />
+                    <BookingsPagination sx={{ height: 'auto' }} />
+                </Box>
             </Box>
-        </Box>
+        </ThemeProvider>
     )
 }
 
